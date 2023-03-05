9b80fd134c0b3f7c74f1990dabca95c63302584d
rinalds14 <39654754+rinalds14@users.noreply.github.com>
2023-03-05 14:58:42 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,25 +1,35 @@
-# python3
-
 import sys
 import threading
 
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    # izveido koku
+    children = [[] for _ in range(n)]
+    for i in range(n):
+        parent = parents[i]
+        if parent == -1:
+            root = i
+        else:
+            children[parent].append(i)
+
+    # koka augstums 
+    def compute_depth(node):
+        if not children[node]:
+            return 1
+        max_depth = 0
+        for child in children[node]:
+            depth = compute_depth(child)
+            max_depth = max(max_depth, depth)
+        return max_depth + 1
+
+    return compute_depth(root)
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
+    n = int(input())
+    parents = list(map(int, input().split()))
+
+    print(compute_height(n, parents))
 
 
 # In Python, the default limit on recursion depth is rather low,
